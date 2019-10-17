10477666ede5505bfa77341209968f3b11c08425
laloehm <laloehm1@gmail.com>
2019-10-17 09:56:20 -0500

    Se agrego animacion a la barra de menu al hacer scroll.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 $(window).scroll(function () {
     let navbar = $("#navbar");
+    let logo = $('.logo');
     if ($("#navbar").offset().top > 300) {
         // $("#navbar").removeClass("navbar-expand-lg");
         $(navbar).css('background', '#4948d2');
+        $(logo).css('width', '70px');
+        $('.navbar-collapse').removeClass('mb-lg-5');
     } else {
         // $("#navbar").addClass("navbar-expand-lg");
         $(navbar).css("background", "transparent");
+        $(logo).css('width', '150px')
+        $('.navbar-collapse').addClass('mb-lg-5');
     }
 });
