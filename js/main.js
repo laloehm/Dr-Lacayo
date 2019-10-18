3449bd7561868e196ed4b88ec9818badf76d5f4e
laloehm <laloehm1@gmail.com>
2019-10-18 14:17:39 -0500

    Cambios

--- js/main.js
+++ js/main.js
@@ -4,15 +4,17 @@ $(window).scroll(function () {
     if ($("#navbar").offset().top > 300) {
         // $("#navbar").removeClass("navbar-expand-lg");
         $(navbar).css('background', '#4948d2');
-        $('.nav-link').css('color', '#fff');
+        // $('.nav-link').css('color', '#fff');
+        $('.nav-link').addClass('active');
         // $(logo).css('width', '60px');
-        $('.navbar-collapse').removeClass('mb-lg-5');
+        $('.navbar-collapse').removeClass('mb-lg-3');
     } else {
         // $("#navbar").addClass("navbar-expand-lg");
         $(navbar).css("background", "transparent");
         // $(logo).css('width', '150px')
-        $('.nav-link').css('color', '#ad0af9');
-        $('.navbar-collapse').addClass('mb-lg-5');
+        // $('.nav-link').css('color', '#ad0af9');
+        $('.nav-link').removeClass('active');
+        $('.navbar-collapse').addClass('mb-lg-3');
     }
 });
 
