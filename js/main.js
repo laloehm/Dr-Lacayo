7da3902c7fb5e6b12d1723ac2ddd714b35a277f7
laloehm <laloehm1@gmail.com>
2019-10-18 10:09:11 -0500

    Se empezo con el diso responsive

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ $(window).scroll(function () {
     let logo = $('.logo');
     if ($("#navbar").offset().top > 300) {
         // $("#navbar").removeClass("navbar-expand-lg");
-        // $(navbar).css('background', '#4948d2');
-        $(navbar).css('background', '#fff');
+        $(navbar).css('background', '#4948d2');
+        $('.nav-link').css('color', '#fff');
         // $(logo).css('width', '60px');
         $('.navbar-collapse').removeClass('mb-lg-5');
     } else {
         // $("#navbar").addClass("navbar-expand-lg");
         $(navbar).css("background", "transparent");
         // $(logo).css('width', '150px')
+        $('.nav-link').css('color', '#ad0af9');
         $('.navbar-collapse').addClass('mb-lg-5');
     }
 });
